7465b3b8a5ef7c2c89a306ed4641a523fcd64fa8
AS <arssoo569@gmail.com>
2024-06-21 21:39:19 +0200

    improving handling of select for Table component

--- src/components/Table2.js
+++ src/components/Table2.js
@@ -7,18 +7,27 @@ import { getTableById } from '../../redux/tablesRedux';
 import { useParams } from 'react-router-dom';
 import { useState, useMemo, useEffect } from 'react';
 
-
 const extractValues = (statuses, status) => {
+    console.log('statuses inside memo', statuses);
     let names = statuses.map(status => status.name);
+    let indexToRemove = names.indexOf(status);
+    if (indexToRemove !== -1) {
+      names.splice(indexToRemove, 1);
+    }
+    names.unshift(status);
     return names;
   };
+
 
 const Table = () => {
 
     
     const {id} = useParams();
     const table = useSelector(state => getTableById(state, id));
+    console.log('table', table);
+    console.log('table status', table.status);
     const statuses = useSelector(getAllStatuses);
+    console.log('HOME statuses:', statuses);
 
     
 
@@ -26,12 +35,17 @@ const Table = () => {
     const [peopleAmount, setPeopleAmount] = useState(table.peopleAmount);
     const [maxPeopleAmount, setMaxPeopleAmount] = useState(table.maxPeopleAmount);
     const [bill, setBill] = useState(table.bill);
-    const [selectionStatuses, setSelectionStatuses] = useState(extractValues(statuses, status));    
 
-    const handleSelectChange = (event) => {
-        event.preventDefault();
-        setStatus(event.target.value);
-      };
+    const [selectionStatuses, setSelectionStatuses] = useState(extractValues(statuses, status));
+
+    // Memoize the result of extractValues
+  const memoizedValues = useMemo(() => extractValues(statuses, status), [statuses, status]);
+
+  // Update selectionStatuses when memoizedValues changes
+  useEffect(() => {
+    setSelectionStatuses(memoizedValues);
+  }, [memoizedValues]);
+
 
     return (
         <Container>
@@ -51,7 +65,7 @@ const Table = () => {
                         Status:
                     </h2>
                     
-                    <select className="mx-4" value={status} onChange={handleSelectChange}>
+                    <select className="mx-4">
                         {
                             selectionStatuses.map(status => (
                                 <option key={status} value={status}>
