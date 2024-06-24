d32d6f354b8d670949034641321a10dcb6f5148a
AS <arssoo569@gmail.com>
2024-06-24 20:11:48 +0200

    next attempt2

--- server.mjs
+++ server.mjs
@@ -1,39 +1,15 @@
 import jsonServer from 'json-server';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import process from 'process';
-
-// Get the __dirname equivalent in ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Create the server
 const server = jsonServer.create();
-
-// Specify the path to the database
-const router = jsonServer.router(path.join(__dirname, 'build/db/app.json'));
-
-// Use default middlewares (static and noCors options)
+const router = jsonServer.router('build/db/app.json');
 const middlewares = jsonServer.defaults({
-  static: path.join(__dirname, 'build'),
+  static: 'build',
   noCors: true
 });
-
-// Set the port
 const port = process.env.PORT || 3131;
-
-// Use the middlewares
 server.use(middlewares);
-
-// Rewriter to handle /api/* routes
 server.use(jsonServer.rewriter({
   '/api/*': '/$1'
 }));
 
-// Use the router
 server.use(router);
-
-// Start the server
-server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
-});
+server.listen(port);
